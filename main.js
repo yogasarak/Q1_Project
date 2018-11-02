5980dbb4129b53433bcc2b55f0c5604714293cec
Sara Keyser <yogasara@icloud.com>
2018-11-02 16:24:51 -0600

    sample recipe card + wireframe image of site

---
+++ main.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // dropdown
+  let elems = document.querySelectorAll('.dropdown-trigger')
+  let instances = M.Dropdown.init(elems)
+
+  // parallax
+  let elemsParallax = document.querySelectorAll('.parallax');
+  let instances2 = M.Parallax.init(elemsParallax);
+
+  // carousel
+  // let carousel = document.querySelectorAll('.carousel')
+  // let inItCarousel = M.Carousel.init(carousel)
+
+
+  // list items in drop down
+  let li = document.querySelectorAll('li')
+
+  for (let i = 0; i < li.length; i++) {
+    let item = li[i]
+
+
+    item.addEventListener('click', function() {
+      console.log(item, 'here')
+    })
+
+  }
+
+
+
+
+
+
+
+
+
+})
