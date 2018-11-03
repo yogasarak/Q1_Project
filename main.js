86a2e3f0c1baf45ae7956d23ebba49f29babe441
Sara Keyser <yogasara@icloud.com>
2018-11-03 15:34:29 -0600

    inner card color add

--- main.js
+++ main.js
@@ -7,29 +7,16 @@ document.addEventListener('DOMContentLoaded', function() {
   let elemsParallax = document.querySelectorAll('.parallax');
   let instances2 = M.Parallax.init(elemsParallax);
 
-  // carousel
-  // let carousel = document.querySelectorAll('.carousel')
-  // let inItCarousel = M.Carousel.init(carousel)
-
-
   // list items in drop down
   let li = document.querySelectorAll('li')
 
   for (let i = 0; i < li.length; i++) {
-    let item = li[i]
+  // let item = li[i]
 
-    item.addEventListener('click', function() {
-
-    })
-
+    item.addEventListener('click', function() {})
   }
 
 
-
-
-
-
-
-
-
+  let hideDiv = document.getElementById('hideDiv')
+    hideDiv.style.display = 'none'
 })
