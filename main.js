2a51780da0b34c8f2f83ba3fecc916ab0fad5d53
Sara Keyser <yogasara@icloud.com>
2018-11-03 16:02:53 -0600

    js show/hide eventListener not working on cards..yet

--- main.js
+++ main.js
@@ -2,21 +2,31 @@ document.addEventListener('DOMContentLoaded', function() {
   // dropdown
   let elems = document.querySelectorAll('.dropdown-trigger')
   let instances = M.Dropdown.init(elems)
-
   // parallax
   let elemsParallax = document.querySelectorAll('.parallax');
   let instances2 = M.Parallax.init(elemsParallax);
-
   // list items in drop down
   let li = document.querySelectorAll('li')
 
-  for (let i = 0; i < li.length; i++) {
-  // let item = li[i]
 
-    item.addEventListener('click', function() {})
+
+  for (let i = 0; i < li.length; i++) {
+    let item = li[i]
+
+
+  item.addEventListener('click', function() {
+    let hideDiv = document.getElementById('hideDiv')
+    let moodyCard = document.getElementById('moodyCard')
+
+    let moody = document.getElementById('moody')
+    if (item === moody) {
+      hideDiv.style.display ='show'
+    } else {
+      hideDiv.style.display = 'none'
+    }
+
+  })
   }
 
 
-  let hideDiv = document.getElementById('hideDiv')
-    hideDiv.style.display = 'none'
 })
